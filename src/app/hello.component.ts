6cd61d3effaad7f9333a0b51716d3026ca69d0ae
dschemmel <dschemmel@ingschepar.de>
2020-07-08 10:06:53 +0200

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, ContentChild, Input, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{Item.name}}!</h1>`,
+  template: `
+    <h1>Hello {{Item.name}}!</h1>
+    <ng-container [ngTemplateOutlet]="top" [ngTemplateOutletContext]="{ $implicit: Item }"></ng-container>
+  `,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent<T extends { name: string }>  {
+  @ContentChild(TemplateRef) top: TemplateRef<any>;
   @Input() Item: T;
 }
